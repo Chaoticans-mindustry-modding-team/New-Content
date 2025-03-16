54a6930440e8f3eb72d773369307a306a766e8a0
rylid-umil <81439905+rylid-umil@users.noreply.github.com>
2025-03-16 00:33:02 -1000

    fix bug :(

--- scripts/main.js
+++ scripts/main.js
@@ -14,11 +14,11 @@ let seq = Vars.tree.loadMusic("seq"); //seq
 let game10 = Vars.tree.loadMusic("game10"); //fanmade
 
 Events.on(ClientLoadEvent, e => {
-  vars.control.sound.ambientMusic.add(day2);
-  vars.control.sound.ambientMusic.add(day3);
-  vars.control.sound.ambientMusic.add(day4);
+  Vars.control.sound.ambientMusic.add(day2);
+  Vars.control.sound.ambientMusic.add(day3);
+  Vars.control.sound.ambientMusic.add(day4);
   Vars.control.sound.darkMusic.add(game10);
-  vars.control.sound.bossMusic.add(seq); //duck it, seq is boss music
+  Vars.control.sound.bossMusic.add(seq); //duck it, seq is boss music
   //duck it, seq is boss music
   //duck it, seq is boss music
   //duck it, seq is boss music

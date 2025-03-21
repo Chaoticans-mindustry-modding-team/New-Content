3dbbcbd4a1e05fe0d7565a5ae84998edb34703a8
rylid-umil <81439905+rylid-umil@users.noreply.github.com>
2025-03-20 17:37:09 -1000

    uptademusics

--- scripts/main.js
+++ scripts/main.js
@@ -12,14 +12,16 @@ let day3 = Vars.tree.loadMusic("day3"); //ye
 let day4 = Vars.tree.loadMusic("day4"); //ye
 let seq = Vars.tree.loadMusic("seq"); //seq
 let game10 = Vars.tree.loadMusic("game10"); //fanmade
+let boss3 = Vars.tree.loadMusic("boss3"); //fanmade boss
 
 Events.on(ClientLoadEvent, e => {
   Vars.control.sound.ambientMusic.add(day2);
-  Vars.control.sound.ambientMusic.add(day3);
-  Vars.control.sound.ambientMusic.add(day4);
+  Vars.control.sound.darkMusic.add(day3);
+  Vars.control.sound.darkMusic.add(day4);
   Vars.control.sound.darkMusic.add(game10);
   Vars.control.sound.bossMusic.add(seq); //duck it, seq is boss music
   //duck it, seq is boss music
   //duck it, seq is boss music
   //duck it, seq is boss music
+  Vars.control.sound.darkMusic.add(seq);
 });

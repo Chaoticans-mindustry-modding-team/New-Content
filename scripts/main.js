cf7297efb01c680d66027bc42fdf4fb626ee5615
Rylid-Umil <81439905+rylid-umil@users.noreply.github.com>
2025-07-22 23:37:57 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -6,15 +6,21 @@ Events.run(ClientLoadEvent, () => {
   let moddedLiquid = contentName => Vars.content.liquid(contentName);
   let moddedPlanet = contentName => Vars.content.planet(contentName);
   // if needed for blocks or units or basically stuff that isnt an item, make a new function later but for now only Items. or add a parameter
-  function addNode(contentChild, contentParent, index) {
+  function addNode(contentChild, contentParent, index, planet) {
     if(contentChild == null || contentParent == null) return;
     if(index == null) index = 0;
 
     contentParent.techNodes.get(index).children.add(
       TechTree.nodeProduce(contentChild, () => {}),
     );
-    contentChild.shownPlanets.addAll(contentParent.shownPlanets);
-    contentChild.databaseTabs.addAll(contentParent.databaseTabs);
+    // If the content you are branching off of also appears on other planets but you only want it to appear on a certain planet, then use planet parameter
+    if (planet == null) {
+      contentChild.shownPlanets.addAll(contentParent.shownPlanets);
+      contentChild.databaseTabs.addAll(contentParent.databaseTabs);
+    } else {
+      contentChild.shownPlanets.addAll(planet);
+      contentChild.databaseTabs.addAll(planet);
+    };
   };
 
   // Application of methods
@@ -22,7 +28,7 @@ Events.run(ClientLoadEvent, () => {
   addNode(Liquids.water, moddedItem("newunits-iron"));
   addNode(Liquids.cryofluid, Liquids.water, 2);
   addNode(Liquids.slag, moddedItem("newunits-iron"));
-  addNode(moddedLiquid("newunits-poison-water"), Liquids.water, 2);
-  addNode(moddedLiquid("newunits-poison"), Liquids.water, 2);
-  addNode(moddedItem("newunits-selevite"), moddedLiquid("newunits-poison"));
+  addNode(moddedLiquid("newunits-poison-water"), Liquids.water, 2, clexon);
+  addNode(moddedLiquid("newunits-poison"), Liquids.water, 2, clexon);
+  addNode(moddedItem("newunits-selevite"), moddedLiquid("newunits-poison"), null, clexon);
 });

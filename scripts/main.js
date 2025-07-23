7d627aaf6fb4b3aca9f93f68f6609859192a05c4
Rylid-Umil <81439905+rylid-umil@users.noreply.github.com>
2025-07-23 01:38:22 -0700

    no sili

--- scripts/main.js
+++ scripts/main.js
@@ -33,5 +33,4 @@ Events.run(ClientLoadEvent, () => {
   addNode(moddedLiquid("newunits-poison"), Liquids.water, -1, clexon);
   addNode(moddedItem("newunits-selevite"), moddedLiquid("newunits-poison"), null, clexon);
   addNode(Items.coal, moddedItem("newunits-scorchonite"));
-  addNode(Items.silicon, Items.coal, -1);
 });

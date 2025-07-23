d957df18bddae6c9ecfd7ce7bfda69b2a66ee62a
Rylid-Umil <81439905+rylid-umil@users.noreply.github.com>
2025-07-22 17:37:21 -0700

    Music move (#21)
    * Remove Music
    
    * remove music's code
    
    * re-add landing
    
    * Move
    
    * notice

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,4 @@
 // noo now this is a java mod /j
-// credits to  NEON-XZR and meep of faith
-/* let mod = Vars.mods.locateMod("newunits");
-mod.meta.displayName = Core.bundle.get("mod.name");
-mod.meta.description = Core.bundle.get("mod.description");
-// special thanks to meep for the help!!
-*/
-//is this relevant?^^^
-let day2 = Vars.tree.loadMusic("day2"); //new-world creepy vibes like / alternate
-let day3 = Vars.tree.loadMusic("day3"); //ye
-let day4 = Vars.tree.loadMusic("day4"); //ye
-let seq = Vars.tree.loadMusic("seq"); //seq
-let game10 = Vars.tree.loadMusic("game10"); //fanmade
-let boss3 = Vars.tree.loadMusic("boss3"); //fanmade boss
-
-Events.on(ClientLoadEvent, e => {
-  Vars.control.sound.ambientMusic.add(day2);
-  Vars.control.sound.darkMusic.add(day3);
-  Vars.control.sound.darkMusic.add(day4);
-  Vars.control.sound.darkMusic.add(game10);
-  Vars.control.sound.bossMusic.add(boss3);
-  Vars.control.sound.bossMusic.add(seq); //duck it, seq is boss music
-  //duck it, seq is boss music
-  //duck it, seq is boss music
-  //duck it, seq is boss music
-  Vars.control.sound.darkMusic.add(seq);
-});
 // Thanks to maboroshiX | Reind
 Events.run(ClientLoadEvent, () => {
   // Definition of methods
